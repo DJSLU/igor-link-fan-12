4dd036baf4db2303dff07988898cde211cefbdfc
emmanuil larin <112695623+DJSLU@users.noreply.github.com>
2022-12-26 14:11:53 +0300

    Add files via upload

---
+++ KONTROLNYA/class.py
@@ -0,0 +1,26 @@
+class Person():
+    def __init__(self,oleg, age):
+        self.name = oleg
+        self.age = age
+
+    def printinfo(self):
+        print(self.name,self.age)
+    name =input()
+    if name =='Не Олег':
+        print('Я не Эммануил, а Олег')
+
+    elif name == 'Олег':
+        print('Олег')
+
+
+
+
+
+
+
+
+
+
+
+
+
